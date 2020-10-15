2ee4e8ccb8e931a1ef934a0d0819cc09e81a4e04
Alexander Bowers <7319abowers@gmail.com>
2020-10-15 22:01:58 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -21,11 +21,32 @@ def nyc_pigeon_organizer(pigeon_data)
   gender_array = data_in_array[1][1].to_a
   lives_array = data_in_array[2][1].to_a
 
+  color_array.each do |color|
+    if color[0].class == Symbol
+      color[0] = color[0].to_s
+    end
+  end
+
+  gender_array.each do |gender|
+    if gender[0].class == Symbol
+      gender[0] = gender[0].to_s
+    end
+ end
 
 pigeon_list.each do |name, value|
    color_array.each do |color|
      if color[1].include? (name)
         pigeon_list[name][:color] << color[0]
+      end
+    end
+    gender_array.each do |gender|
+      if gender[1].include? (name)
+        pigeon_list[name][:gender] << gender[0]
+      end
+    end
+    lives_array.each do |lives|
+      if lives[1].include? (name)
+        pigeon_list[name][:lives] << lives[0]
       end
     end
  end
