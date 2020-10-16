971a811c985b487795ff9f1c62662f374ac9e200
Dana Bliss <danajackson2@gmail.com>
2020-10-16 21:11:56 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each_with_object({}) do |(key, value), names_array|
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !names_array[name]
+          names_array[name] = {}
+        end
+        if !names_array[name][key]
+          names_array[name][key] = []
+        end  
+        names_array[name][key].push(inner_key.to_s)
+      end  
+    end  
+  end  
 end
