969d166d6e021b9d94b46875b600973528cabc25
Riley Iverson <rmiverson99@gmail.com>
2020-10-30 04:14:05 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = Hash.new
+  
+  data.map { |l1_key, l1_value|
+    l1_value.map { |l2_key, l2_value|
+      l2_value.map { |l3_index|
+        if !new_hash[l3_index]
+          new_hash[l3_index] = {}
+        end
+        if !new_hash[l3_index][l1_key]
+          new_hash[l3_index][l1_key] = []
+        end
+        new_hash[l3_index][l1_key].push(l2_key.to_s)
+      }
+    }
+  }
+  return new_hash
 end
